6298391f6a3a25976e5f37e9b57cea973696965b
Caio Pereira <caiopereira1998@hotmail.com>
2024-10-21 17:54:12 -0300

    remoção de serviço não utilizado

--- lib/services/financial_box_service.dart
+++ lib/services/financial_box_service.dart
@@ -30,15 +30,6 @@ class FinancialBoxService {
         .snapshots();
   }
 
-  Stream<QuerySnapshot> findMyFinancialBoxAno(String userId) {
-    return FirebaseFirestore.instance
-        .collection('my_financial_box')
-        .doc(userId)
-        .collection('financial_box')
-        .where('tipoCaixaSelecionado', isEqualTo: 'Saída')
-        .snapshots();
-  }
-
   void saveFinancialBox(String idFinancialBox, String uid, FinancialBox newFinancialBox) async {
     await FirebaseFirestore.instance
         .collection('financial_box')
