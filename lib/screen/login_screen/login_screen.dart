3f73fd4f98c29412c913382469909d7d29f884f0
Caio Pereira <caio.moreira@partners.mv.com.br>
2025-03-06 18:28:37 -0300

    Alterando o Layout do Login

--- lib/screen/login_screen/login_screen.dart
+++ lib/screen/login_screen/login_screen.dart
@@ -24,127 +24,230 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: Container(
-          color: Colors.teal,
-          padding: const EdgeInsets.all(16),
+      backgroundColor: Colors.teal,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SingleChildScrollView(
-                padding: const EdgeInsets.all(16.0),
+             Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text.rich(
+                      TextSpan(
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'Leal Financial',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            )
+                          ),
+                        ]
+                      )
+                    ),
+                    const SizedBox(height: 16,),
+                    Container(
+                      width: 200,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            blurRadius: 10,
+                            spreadRadius: 5,
+                            offset: const Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: ClipOval(
+                        child: Image.asset(
+                          "imagens/logo.png",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                 ),
+                ),
+              
+              //Formulário
+              Form(
+                key: _formKey,
                 child: Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 40
+                  ),
+                  decoration: const BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(16),
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(45)
+                    ),
                   ),
                   child: Column(
-                    children: [
-                      const Text(
-                        'Bem Vindo!', style: TextStyle(fontSize: 30),
-                      ),
-                      const SizedBox(height: 40),
+                     crossAxisAlignment: CrossAxisAlignment.stretch,
+                     children: [
+                
+                      //Email
                       TextFormField(
                         controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
+                          keyboardType: TextInputType.emailAddress,
                         decoration: CustomInputDecoration.build(
-                          labelText: 'E-mail',
-                          hintText: 'Digite seu E-mail',
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Por favor, digite seu E-mail!';
-                          } else if (!value.contains('@')) {
-                            return 'Por favor, digite um E-mail válido!';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 16),
-                      TextFormField(
-                          controller: _senhaController,
-                          obscureText: _senhaVisivel,
-                          decoration: CustomInputDecoration.build(
-                            labelText: 'Senha',
-                            hintText: 'Digite sua Senha',
-                            suffixIcon: IconButton(
-                              padding: const EdgeInsets.only(right: 3),
-                              icon: Icon(
-                                _senhaVisivel
-                                    ? Icons.visibility_off
-                                    : Icons.visibility,
-                              ),
-                              onPressed: () {
-                                setState(() {
-                                  _senhaVisivel = !_senhaVisivel;
-                                });
-                              },
-                            ),
+                            labelText: 'E-mail',
+                            hintText: 'Digite seu E-mail',
                           ),
                           validator: (value) {
                             if (value!.isEmpty) {
-                              return 'Por favor, digite sua Senha!';
+                              return 'Por favor, digite seu E-mail!';
+                            } else if (!value.contains('@')) {
+                              return 'Por favor, digite um E-mail válido!';
                             }
                             return null;
-                          }
+                          },
                       ),
                       const SizedBox(height: 16),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            _login();
-                          }
-                        },
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                
+                      //Senha
+                      TextFormField(
+                        controller: _senhaController,
+                            obscureText: _senhaVisivel,
+                            decoration: CustomInputDecoration.build(
+                              labelText: 'Senha',
+                              hintText: 'Digite sua Senha',
+                              suffixIcon: IconButton(
+                                padding: const EdgeInsets.only(right: 3),
+                                icon: Icon(
+                                  _senhaVisivel
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _senhaVisivel = !_senhaVisivel;
+                                  });
+                                },
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Por favor, digite sua Senha!';
+                              }
+                              return null;
+                            }
+                      ),
+                      const SizedBox(height: 16),
+                
+                      //Botão de Entrar
+                      SizedBox(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            )
+                          ),
+                          onPressed: (){
+                            if (_formKey.currentState!.validate()) {
+                                _login();
+                              }
+                          }, 
+                          child: const Text(
+                            'Entrar', 
+                            style: TextStyle(
+                              fontSize: 18
+                              ),
+                            )
+                          ),
+                      ),
+                      const SizedBox(height: 16),
+                
+                      // Esqueceu a senha
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: (){
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return const ResetPasswordModal();
+                              },
+                            );
+                          }, 
+                        child: const Text(
+                          'Esqueceu a senha?',
+                          style: TextStyle(
+                            color: Colors.teal,
+                            ),
+                          )
+                        ),
+                      ),
+                
+                      // Divisor
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Row(
                           children: [
-                            Text(
-                              "Entrar",
-                              style: TextStyle(
-                                fontSize: 20,
+                            Expanded(
+                              child: Divider(
+                                color: Colors.grey.withAlpha(90),
+                                thickness: 2,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 15),
+                              child: Text('ou'),
+                            ),
+                            Expanded(
+                              child: Divider(
+                                color: Colors.grey.withAlpha(90),
+                                thickness: 2,
                               ),
                             ),
                           ],
                         ),
                       ),
-                      const SizedBox(height: 16),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => RegisterScreen(),
+                
+                      //Botão de novo usuário
+                      SizedBox(
+                        child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18)
                             ),
-                          );
-                        },
-                        child: const Text(
-                          'Não tem conta?, clique aqui',
-                          style: TextStyle(
-                            fontSize: 20,
+                            side: const BorderSide(
+                              width: 2,
+                              color: Colors.teal
+                            )
                           ),
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return ResetPasswordModal();
-                            },
-                          );
-                        },
-                        child: const Text(
-                          'Equeceu sua senha?',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ),
-                    ],
+                          onPressed: (){
+                           Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const RegisterScreen(),
+                                  ),
+                             );
+                          }, 
+                          child: const Text(
+                            'Criar conta',
+                            style: TextStyle(
+                              fontSize: 18
+                            ),
+                            )
+                          ),
+                      )
+                     ], 
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
